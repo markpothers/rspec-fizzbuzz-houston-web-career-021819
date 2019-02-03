644258a1cccbb16738ecfc33e9170b6894f79c41
Mark Pothecary <markpothers@hotmail.com>
2019-02-03 22:54:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num / 3 == (num/3).floor && num / 5 ==(num/5).floor
+  if num / 3 == (num/3).floor && num / 5 == (num/5).floor
     return "FizzBuzz"
   elsif num / 3 == (num/3).floor
     return "Fizz"
