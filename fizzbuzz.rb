04b3fda482d595eb0721512e6ab7a23cec5b5fc4
Mark Pothecary <markpothers@hotmail.com>
2019-02-03 23:01:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if (num.000 / 3.000 == (num.000/3.000).floor) && (num.000 / 5.000 == (num.000/5.000).floor)
+  if (num / 3.000 == (num/3.000).floor) && (num / 5.000 == (num/5.000).floor)
     return "FizzBuzz"
-  elsif (num.000 / 3.000 == (num.000/3.000).floor)
+  elsif (num / 3.000 == (num/3.000).floor)
    return "Fizz"
-  elsif (num.000 / 5.000 ==(num.000/5.000).floor)
+  elsif (num / 5.000 ==(num/5.000).floor)
     return "Buzz"
   else
     return nil
